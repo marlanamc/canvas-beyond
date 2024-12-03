a9b803fc9463bb0d70a93a456ce2096b80fa7b2a
Marlana Creed <marlanacreed@Marlanas-MacBook-Pro.local>
2024-12-02 22:18:37 -0500

    Convert next.config from TypeScript to JavaScript

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['picsum.photos'], // if you're using external image domains
+  },
+}
+
+module.exports = nextConfig 
